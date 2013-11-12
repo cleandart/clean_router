156bb9927ce8f7eb9e9daac0fa11899a963f5d37
Peter Csiba <petherz@gmail.com>
2013-11-12 14:52:55 +0100

    Separating to common and client

--- lib/client.dart
+++ lib/client.dart
@@ -6,6 +6,7 @@ library vacuum.router;
 import "dart:core";
 import "dart:async";
 import 'package:clean_data/clean_data.dart';
+import 'common.dart';
 
 /**
  * [View] is responsible for manipulating data received from server to be used for HTML.
@@ -29,144 +30,6 @@ abstract class View {
 }
 
 /**
- * Maps [Map] of variables to [String] Url component and vice versa.
- */
-class Route {
-  RegExp _matchExp;
-  List<String> _variables = [];
-  List _urlParts = [];
-
-  /**
-   * Constructs [Route] using [String] pattern.
-   *
-   * Pattern is divided to parts by slash '/' character. The slash must be the
-   * very first character of the pattern. Each part can be either static or
-   * placeholder. Static part can contain arbitrary number of [a-zA-Z0-9_-]
-   * characters. Placeholder part consists of variable name enclosed in curly
-   * braces. Variable name consists of [a-zA-Z0-9_] characters, with the first
-   * character being a letter.
-   */
-  Route(pattern) {
-    if (pattern.isEmpty || pattern[0] != '/') {
-      throw new FormatException("Url pattern has to begin with '/' character.");
-    }
-    if (pattern[pattern.length - 1] != '/') {
-      throw new FormatException("Url pattern has to end with '/' character.");
-    }
-
-    RegExp exp = new RegExp(r"^(?:([\w-]*)|{([^{}]*)})$");
-    var matcherParts = new List();
-    var parts = pattern.split('/');
-    for (var part in parts) {
-      var match = exp.firstMatch(part);
-      if (match == null) {
-        throw new FormatException(
-            """Only alphanumeric characters, dash '-' and underscore '_'
-             are allowed in the URL."""
-        );
-      }
-      if (match.group(1) != null) {
-        var group = match.group(1);
-        matcherParts.add(group);
-        _urlParts.add({'value': group, 'isVariable': false});
-      } else {
-        var group = match.group(2);
-        matcherParts.add("([^/]*)");
-        _variables.add(group);
-        _urlParts.add({'value': group, 'isVariable': true});
-      }
-    }
-
-    _matchExp = new RegExp(r"^" + matcherParts.join('/') + r"$");
-  }
-  /**
-   * Matches the [url] against the [Route] pattern and returns [Map] of matched.
-   * This is the inverse function to [Route.path].
-   */
-  Map match(String url) {
-    var match = _matchExp.firstMatch(url);
-
-    // If the [url] does not match, returns [null].
-    if (match == null) {
-      return null;
-    }
-
-    // Decode [url] parameters and fill them into [Map].
-    Map result = new Map();
-    for (var i = 0; i < _variables.length; i++) {
-      result[_variables[i]] = Uri.decodeComponent(match.group(i + 1));
-    }
-
-    return result;
-  }
-
-  /**
-   * Constructs the [url] using the [Route] pattern and values in [variables].
-   * This is theiInverse function to [Route.match].
-   * Accepts both [Map] and [Data] as parameters.
-   * All [variables] of [Route] must be provided.
-   */
-  String path(dynamic variables) {
-    var parts = [];
-    for (var part in this._urlParts) {
-      var value = part['value'];
-      if (part['isVariable']) {
-        value = variables[value];
-        if (value == null) {
-          throw new ArgumentError("Missing value for ${part['value']}.");
-        }
-      }
-      parts.add(Uri.encodeComponent(value));
-    }
-    return parts.join('/');
-  }
-}
-
-/**
- * [Router] consists of multiple named [Route]s and provides methods for
- * translating [Route]s to url/path and vice versa.
- */
-class Router {
-  final Map<String, Route> routes;
-  String host;
-
-  Router(this.host, this.routes);
-
-  /**
-   * Returns path part of the url corresponding to the given [routeName] and
-   * [parameters]. Accepts [Map] and [Data] as [parameters].
-   */
-  String routePath(String routeName, dynamic parameters) {
-    var route = this.routes[routeName];
-    if (route == null) {
-      throw new ArgumentError('Router does not contain a route "$routeName".');
-    }
-    return this.routes[routeName].path(parameters);
-  }
-
-  /**
-   * Returns the whole url corresponding to the given [routeName] and
-   * [parameters]. Accepts both [Map] and [Data] as [parameters].
-   */
-  String routeUrl(String routeName, dynamic parameters) {
-    return this.host + this.routePath(routeName, parameters);
-  }
-
-  /**
-   * Returns the List [[routeName, matchedParameters]] matching the [url].
-   */
-  List match(String url) {
-    for (var key in this.routes.keys) {
-      var match = this.routes[key].match(url);
-      if (match != null) {
-        return [key, match];
-      }
-    }
-    return null;
-  }
-}
-
-/**
  * [PageNavigator] wires together [History] management, [Route] matching and
  * [View] rendering. It manages url addresses and bind them to views.
  * It updates url (replaceState, pushState) if the url params are changed in the view.
@@ -178,9 +41,9 @@ class PageNavigator {
   Data _activeParameters;
   StreamSubscription _dataSubscription;
   final Map _views = {};
-  
+
   String _activePath = null;
-  String get activePath => _activeRouteName == null || _activeRouteName == 'default' 
+  String get activePath => _activeRouteName == null || _activeRouteName == 'default'
       ? _activePath : _router.routePath(_activeRouteName, _activeParameters);
 
 /**
@@ -201,9 +64,9 @@ class PageNavigator {
     }
     _views[routeName] = view;
   }
-  
+
 /**
- * Registers a [View] which is called when router does not find any match.  
+ * Registers a [View] which is called when router does not find any match.
  */
   void registerDefaultView(View view) {
     _views['default'] = view;
@@ -221,7 +84,7 @@ class PageNavigator {
  */
   void navigate(String routeName, Map parameters, {bool pushState: false}) {
     //== prepare variables
-    
+
     if(!_views.containsKey(routeName)) {
       throw new ArgumentError("View not found for '$routeName'");
     }
@@ -229,12 +92,12 @@ class PageNavigator {
     if (_views[_activeRouteName] != _views[routeName]) {
       _setActiveParameters(parameters);
       _handleViewTransition(_views[_activeRouteName], _views[routeName]);
-    } 
+    }
     else {
       _activeParameters.removeAll(_activeParameters.keys.toList());
       _activeParameters.addAll(parameters);
     }
-    
+
     _activeRouteName = routeName;
 
     //== update history
@@ -245,14 +108,14 @@ class PageNavigator {
       _updateHistoryState();
     }
   }
-  
+
   void _handleViewTransition(View oldView, View newView) {
     if (oldView != null) {
       oldView.unload();
     }
     newView.load(_activeParameters);
   }
-  
+
   void _setActiveParameters(Map parameters) {
     if(_dataSubscription != null) {
       _dataSubscription.cancel();
@@ -262,7 +125,7 @@ class PageNavigator {
     _dataSubscription = _activeParameters.onChange.listen(
         (ChangeSet change) => _updateHistoryState());
   }
-  
+
 /**
  *  Navigates the browser to the selected Path using [navigate] function.
  */
@@ -276,7 +139,7 @@ class PageNavigator {
       _handleViewTransition(_views[_activeRouteName], _views['default']);
       _activeRouteName = null;
       _activePath = path;
-      
+
       if (pushState) {
         this.pushState();
       }
@@ -285,7 +148,7 @@ class PageNavigator {
       }
     }
   }
-  
+
   void _updateHistoryState() {
     _history.replaceState(new Object(), "", activePath);
   }
