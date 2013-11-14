121367e344bccbeb49950757713e4f5315b236c4
Samuel Hapak <samuel.hapak@gmail.com>
2013-11-14 13:41:31 +0100

    Update LICENSE, AUTHORS and LICENSE comments, names of libraries.

--- lib/client.dart
+++ lib/client.dart
@@ -1,8 +1,8 @@
-// Copyright (c) 2013, Samuel Hapak, Peter Csiba, Jozef Brandys. All rights reserved. Use of this source
-// code is governed by a BSD-style license that can be found in the LICENSE
-// file.
+// Copyright (c) 2013, the Clean project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
-library vacuum.router;
+library clean_router.client;
 import "dart:core";
 import "dart:async";
 import 'package:clean_data/clean_data.dart';
---
+++ test/test_all.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2013, the Clean project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library test_all;
+
+import "common_test.dart" as common_test;
+import "server_test.dart" as server_test;
+import "client_test.dart" as client_test;
+
+void main() {
+  common_test.main();
+  server_test.main();
+  client_test.main();
+}
