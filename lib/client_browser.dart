6c7c2fce219e7c1e9e733e7178806a6dc9bce489
Jozef Brandys <jozef.brandys@gmail.com>
2013-11-18 15:50:13 +0100

    Refactoring.

--- lib/client_browser.dart
+++ lib/client_browser.dart
@@ -8,30 +8,7 @@ import "dart:html";
 import 'client.dart';
 export 'client.dart';
 
-/**
- * Should have the closest approximation of [dart.dom.history] as possible for browsers not supporting [HTML5].
- * This is implemented via tokens after hashes in url which are allowed to change by browsers.
- *
- * See http://api.dartlang.org/docs/releases/latest/dart_html/History.html#pushState .
- */
-
-abstract class History {
-  void pushState(Object data, String title, [String url]);
-  void replaceState(Object data, String title, [String url]);
-}
-
-class HistoryFactory {  
-  dynamic getHistory() {
-    if (window.history.pushState && window.history.replaceState) {
-      return window.history;
-    }
-    else {
-      return new HashHistory();
-    }
-  }
-}
-
-class HashHistory implements History {
+class HashHistory  {
 
   void pushState(Object data, String title, [String url]) {
     window.location.hash = '#' + url;
@@ -42,8 +19,12 @@ class HashHistory implements History {
   }
 }
 
-class PageNavigatorFactory {
-  PageNavigator getPageNavigator(Router router){
-    return new PageNavigator(router, new HistoryFactory().getHistory());
+
+PageNavigator createPageNavigator(Router router){
+  if (window.history.pushState && window.history.replaceState) {
+    return new PageNavigator(router, window.history);
+  }
+  else {
+    return new PageNavigator(router, new HashHistory());
   }
 }
