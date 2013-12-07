47b122e8f69ea7f895c600061f9a40ed346ce791
Samuel Hapak <samuel.hapak@gmail.com>
2013-12-07 15:39:35 +0100

    Add hop runner.

--- test/test_all.dart
+++ test/test_all.dart
@@ -8,7 +8,16 @@ import "common_test.dart" as common_test;
 import "server_test.dart" as server_test;
 import "client_test.dart" as client_test;
 
-void main() {
+import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
+
+main() {
+  run(new VMConfiguration());
+}
+
+run(configuration) {
+  unittestConfiguration = configuration;
+
   common_test.main();
   server_test.main();
   client_test.main();
